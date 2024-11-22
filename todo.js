e59e5bcf7e76553bad8b22f26b76cda9165870c9
Maja Kazmierczak <upgbhvet@students.codeinstitute.net>
2024-11-22 13:40:41 +0000

    Create pseudo-code template for a to-do app

---
+++ todo.js
@@ -0,0 +1,20 @@
+// Heading: Todo
+
+// input field to add new todo
+
+// button to add/submit new todo
+    // Event listener for the add button
+    // Call the add new todo function
+
+// display/list of todos
+
+// button to delete a todo item
+    // Event listener for the delete button
+    // Call the delete todo function
+
+// button to edit to-do note
+    // Event listener for the edit button
+    // Call the edit todo function
+
+
+
